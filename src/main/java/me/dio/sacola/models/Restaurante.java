38cc668ba2b4bc2cde5a34aa4e823ad6948e7f1b
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-10-16 17:01:22 -0300

    Criada as classes Endereco, Produto e Restaurante e seus relacionamentos e teste com BD H2

---
+++ src/main/java/me/dio/sacola/models/Restaurante.java
@@ -0,0 +1,31 @@
+package me.dio.sacola.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Produto> cardapio;
+
+    @Embedded
+    private Endereco endereco;
+
+}
